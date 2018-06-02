1d8197c31df5aa89030304f61bfcbfa4d0c8ce2a
cMing <mayday404@163.com>
2018-06-02 12:56:44 +0800

    修复main入口

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from 'rollup-plugin-babel'
 export default {
   input: './src/lefit-mobile-crop.js',
   output: {
-    file: './dist/lefit-mobile-crop.min.js',
+    file: 'lefit-mobile-crop.min.js',
     format: 'cjs'
   },
   plugins: [
